59a8c46ce930fde3c682ac3f2c6d1b58dc51ea06
joelans <baileyderek59@gmail.com>
2025-01-06 18:05:18 +0300

    feat: implement countCats, deleteDigits and sumDigits functions

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const str = n.toString();
+  let max = -1;
+  for (let i = 0; i < str.length; i += 1) {
+    const number = Number.parseInt(str.substring(0, i) + str.substring(i + 1));
+    max = Math.max(max, number);
+  }
+  return max;
 }
 
 module.exports = {
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  let str = n.toString();
+  while(str.length > 1) {
+    let number = 0;
+    for (let i = 0; i < str.length; i += 1) {
+      number += Number.parseInt(str.charAt(i));
+    }
+    str = number.toString();
+  }
+  return Number.parseInt(str);
 }
 
 module.exports = {
